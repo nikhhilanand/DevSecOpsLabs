2f6c66dc90f163363023136788b695da0edea892
Nikhhil Anand <nikhhilanand@users.noreply.github.com>
2023-04-11 23:44:38 +0530

    Update owasp-dependency-check.sh

--- owasp-dependency-check.sh
+++ owasp-dependency-check.sh
@@ -9,18 +9,18 @@ CACHE_DIRECTORY="$DC_DIRECTORY/data/cache"
 if [ ! -d "$DATA_DIRECTORY" ]; then
     echo "Initially creating persistent directory: $DATA_DIRECTORY"
     mkdir -p "$DATA_DIRECTORY"
+    chmod -R 777 "$DATA_DIRECTORY"
 fi
 if [ ! -d "$CACHE_DIRECTORY" ]; then
     echo "Initially creating persistent directory: $CACHE_DIRECTORY"
     mkdir -p "$CACHE_DIRECTORY"
+    chmod -R 777 "$CACHE_DIRECTORY"
 fi
 
 # Make sure we are using the latest version
 docker pull owasp/dependency-check:$DC_VERSION
 
 docker run --rm \
-    -e user=$USER \
-    -u $(id -u ${USER}):$(id -g ${USER}) \
     --volume $(pwd):/src \
     --volume "$DATA_DIRECTORY":/usr/share/dependency-check/data:z \
     --volume $(pwd)/odc-reports:/report:z \
@@ -31,3 +31,5 @@ docker run --rm \
     --out /report
     # Use suppression like this: (where /src == $pwd)
     # --suppression "/src/security/dependency-check-suppression.xml"
+#    -e user=$USER \
+#    -u $(id -u ${USER}):$(id -g ${USER}) \
